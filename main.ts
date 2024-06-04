687712d518033045628c0fcf0765bf94d9db514c
Mickael Kerbrat <mickael.kerbrat@gmail.com>
2024-06-03 20:34:02 -0700

    added word count for the whole vault option

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
-import { MarkdownView, App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {MarkdownView, App, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile} from 'obsidian';
 import getWordCount from "./getWordCount";
+
+enum WordCountScope {
+	SELECTION	= "SELECTION",
+	VAULT = "VAULT",
+}
 
 interface BeeminderWordCountSettings {
 	userName: string,
@@ -7,6 +12,8 @@ interface BeeminderWordCountSettings {
 	authToken: string,
 	currentWordCnt: number,
 	editingFileTitle: string,
+	scope: WordCountScope,
+
 }
 
 const DEFAULT_SETTINGS: BeeminderWordCountSettings = {
@@ -15,6 +22,7 @@ const DEFAULT_SETTINGS: BeeminderWordCountSettings = {
 	authToken: "",
 	currentWordCnt: 0,
 	editingFileTitle: "",
+	scope: WordCountScope.SELECTION,
 }
 
 export default class BeeminderWordCountPlugin extends Plugin {
@@ -29,14 +37,19 @@ export default class BeeminderWordCountPlugin extends Plugin {
 			id: 'create-word-count-datapoint',
 			name: 'Send word count to Beeminder',
 			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new BeeminderResponseModal(this.app, this.settings).open();
-					}
-					return true;
+				switch(this.settings.scope){
+					case WordCountScope.SELECTION:
+						const leaf = this.app.workspace.activeLeaf;
+						if(! leaf) return false;
+						break;
+					case WordCountScope.VAULT:
+						break;
 				}
-				return false;
+
+				if (!checking) {
+					new BeeminderResponseModal(this.app, this.settings).open();
+				}
+				return true;
 			}
 		});
 
@@ -86,7 +99,7 @@ class BeeminderResponseModal extends Modal {
 		let {contentEl} = this;
 		const result = await this.createDataPoint();
 		if (result.success) {
-			contentEl.setText(`${this.setting.currentWordCnt} word count sent to Beeminder. Good work!`);
+			contentEl.setText(`${result.wordCount} word count sent to Beeminder. Good work!`);
 		} else {
 			contentEl.setText(`Failed to create datapoint. ${result.body}`);
 		}
@@ -100,12 +113,26 @@ class BeeminderResponseModal extends Modal {
 	// Thanks to https://github.com/jamiebrynes7/obsidian-todoist-plugin/blob/master/src/api/api.ts
 	async createDataPoint() {
 		const url = `https://www.beeminder.com/api/v1/users/${this.setting.userName}/goals/${this.setting.goalName}/datapoints.json`;
+		let wordCount = this.setting.currentWordCnt
 
 		let now = new Date();
+		let title = `${now.toISOString()} - `
+		switch(this.setting.scope){
+			case WordCountScope.SELECTION:
+				title += `${this.setting.editingFileTitle}`
+				break;
+			case WordCountScope.VAULT:
+				title += `Vault ${this.app.vault.getName()}`
+				wordCount = await this.countWordInVault()
+				break;
+		}
+
+		console.log(`${wordCount} words in vault [${this.setting.scope}]`)
+
 		let formData = new FormData();
 		formData.append('auth_token', this.setting.authToken);
-		formData.append('value', `${this.setting.currentWordCnt}`);
-		formData.append('comment', `${now.toISOString()} - ${this.setting.editingFileTitle}`);
+		formData.append('value', `${wordCount}`);
+		formData.append('comment', title);
 
 		const response = await fetch(url, {
 			method: "POST",
@@ -114,10 +141,21 @@ class BeeminderResponseModal extends Modal {
 		const text = await response.text();
 
 		return {
+			wordCount,
 			success: response.ok,
 			body: text,
 		}
 	}
+
+	async countWordInVault() {
+		let total_count = 0
+		const contentPromises = this.app.vault.getMarkdownFiles().map( mdF => this.app.vault.read(mdF))
+	 	for(const contentPromise of contentPromises){
+			total_count += getWordCount(await contentPromise)
+		}
+		return total_count
+	}
+
 }
 
 class BeeminderWordCountSettingTab extends PluginSettingTab {
@@ -135,12 +173,28 @@ class BeeminderWordCountSettingTab extends PluginSettingTab {
 
 		containerEl.createEl('h2', {text: 'Settings for the Beeminder word count plugin'});
 
-		this.createSetting('Beeminder auth_token', (val: string) => this.plugin.settings.authToken = val, true);
-		this.createSetting('Beeminder user name', (val: string) => this.plugin.settings.userName = val, false);
-		this.createSetting('Beeminder goal name', (val: string) => this.plugin.settings.goalName = val, false);
+		this.createSetting('Beeminder auth_token', (val: string) => this.plugin.settings.authToken = val, this.plugin.settings.authToken);
+		this.createSetting('Beeminder user name', (val: string) => this.plugin.settings.userName = val, this.plugin.settings.userName);
+		this.createSetting('Beeminder goal name', (val: string) => this.plugin.settings.goalName = val, this.plugin.settings.goalName);
+
+		new Setting(this.containerEl)
+			.setName('Word count Scope')
+			.addDropdown(
+				dropdown => {
+					for(const enumVal in WordCountScope){
+						dropdown.addOption(enumVal, enumVal)
+					}
+					dropdown.setValue(this.plugin.settings.scope)
+					dropdown.onChange((new_v: WordCountScope) => {
+						this.plugin.settings.scope = new_v
+					})
+					return dropdown
+				}
+			)
+
 	}
 
-	createSetting(name: string, pluginFieldSetter: Function, secret: Boolean) {
+	createSetting(name: string, pluginFieldSetter: Function, default_val: string) {
 		const callback = async (val: string) => {
 			pluginFieldSetter(val);
 			await this.plugin.saveSettings();
@@ -148,7 +202,7 @@ class BeeminderWordCountSettingTab extends PluginSettingTab {
 		new Setting(this.containerEl)
 			.setName(name)
 			.addText(text => text
-				.setValue('')
+				.setValue(default_val)
 				.onChange(callback)
 			);
 	}
